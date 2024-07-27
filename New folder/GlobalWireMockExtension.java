5901073e4308a355b134b71bb3e260349507ef06
Anirudh Loya <anirudhloya93@gmail.com>
2024-07-27 23:47:28 +0530

    Updated for graphql stub

--- New folder/GlobalWireMockExtension.java
+++ New folder/GlobalWireMockExtension.java
@@ -1,16 +1,25 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.AfterAllCallback;
-import lombok.extern.slf4j.Slf4j;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 public class GlobalWireMockExtension implements BeforeAllCallback, AfterAllCallback {
+
+    private static final Handlebars handlebars = new Handlebars();
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
@@ -53,9 +62,15 @@ public class GlobalWireMockExtension implements BeforeAllCallback, AfterAllCallb
                 Map<String, String> details = entry.getValue();
                 String mockDataFilePath = details.get("mockDataFilePath");
                 String propertyToUpdate = details.get("propertyToUpdate");
+                String responseFormat = details.get("responseFormat");
 
                 log.info("Setting up stub for endpoint: {}", endpoint);
-                setupStub(endpoint, mockDataFilePath);
+                if (endpoint.endsWith("graphql") && responseFormat != null) {
+                    setupGraphQLStub(endpoint, mockDataFilePath, responseFormat);
+                } else {
+                    setupNormalStub(endpoint, mockDataFilePath);
+                }
+
                 if (propertyToUpdate != null && !propertyToUpdate.isEmpty()) {
                     System.setProperty(propertyToUpdate, WireMockServerManager.getWireMockServer().baseUrl());
                 }
@@ -63,20 +78,50 @@ public class GlobalWireMockExtension implements BeforeAllCallback, AfterAllCallb
         }
     }
 
-    private void setupStub(String endpoint, String mockDataFilePath) {
+    private void setupNormalStub(String endpoint, String mockDataFilePath) throws IOException {
         try (InputStream mockDataStream = getClass().getResourceAsStream("/mockdata/" + mockDataFilePath)) {
             if (mockDataStream != null) {
                 String mockData = IOUtils.toString(mockDataStream, "UTF-8");
-                WireMockServerManager.getWireMockServer().stubFor(com.github.tomakehurst.wiremock.client.WireMock.get(com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo(endpoint))
-                        .willReturn(com.github.tomakehurst.wiremock.client.WireMock.aResponse()
-                                .withHeader("Content-Type", "application/json")
-                                .withBody(mockData)));
+                WireMockServerManager.getWireMockServer().stubFor(
+                    WireMock.get(WireMock.urlEqualTo(endpoint))
+                        .willReturn(WireMock.aResponse()
+                            .withHeader("Content-Type", "application/json")
+                            .withBody(mockData))
+                );
                 log.info("Stubbed endpoint {} with mock data from file {}", endpoint, mockDataFilePath);
             } else {
                 log.error("Mock data file not found: {}", mockDataFilePath);
             }
+        }
+    }
+
+    private void setupGraphQLStub(String endpoint, String mockDataFilePath, String responseFormat) {
+        try (InputStream mockDataStream = getClass().getResourceAsStream("/mockdata/" + mockDataFilePath);
+             InputStream responseFormatStream = getClass().getResourceAsStream("/mockdata/" + responseFormat)) {
+            if (mockDataStream != null && responseFormatStream != null) {
+                String mockData = IOUtils.toString(mockDataStream, "UTF-8");
+                String responseTemplate = IOUtils.toString(responseFormatStream, "UTF-8");
+                ObjectMapper objectMapper = new ObjectMapper();
+                List<Map<String, Object>> mockDataList = objectMapper.readValue(mockData, List.class);
+
+                for (Map<String, Object> mockEntry : mockDataList) {
+                    Template template = handlebars.compileInline(responseTemplate);
+                    String responseBody = template.apply(mockEntry);
+
+                    WireMockServerManager.getWireMockServer().stubFor(
+                        WireMock.post(WireMock.urlEqualTo(endpoint))
+                            .withHeader("Content-Type", WireMock.equalTo("application/json"))
+                            .willReturn(WireMock.aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withBody(responseBody))
+                    );
+                    log.info("Stubbed endpoint {} with mock data from file {}", endpoint, mockDataFilePath);
+                }
+            } else {
+                log.error("Mock data file or response format file not found: {} or {}", mockDataFilePath, responseFormat);
+            }
         } catch (IOException e) {
-            log.error("Failed to setup stub for endpoint {} with mock data file {}: {}", endpoint, mockDataFilePath, e.getMessage());
+            log.error("Failed to setup stub for endpoint {} with mock data file {} and response format {}: {}", endpoint, mockDataFilePath, responseFormat, e.getMessage());
         }
     }
 }
