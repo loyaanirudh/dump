78eb26203ac172ca64f7ac99792c06d5bdabd8dd
loyaanirudh <anirudhloya93@gmail.com>
2024-07-30 11:39:01 +0530

    Update GlobalWireMockExtension.java

--- New folder/GlobalWireMockExtension.java
+++ New folder/GlobalWireMockExtension.java
@@ -4,24 +4,26 @@ import com.github.jknack.handlebars.Template;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@Slf4j
-public class GlobalWireMockExtension implements TestInstancePostProcessor {
+public class GlobalWireMockExtension implements BeforeAllCallback, TestInstancePostProcessor {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalWireMockExtension.class);
     private static final WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
     private static final Handlebars handlebars = new Handlebars();
-    private static final String DEFAULT_RESPONSE_FORMAT = "{\"id\": \"{{id}}\", \"salesClientParentShortName\": \"{{salesClientParentShortName}}\"}";
-    private static final String DEFAULT_NOT_FOUND_RESPONSE_FORMAT = "{\"error\": \"No data found for id {{id}}\"}";
 
     static {
         wireMockServer.start();
@@ -29,27 +31,32 @@ public class GlobalWireMockExtension implements TestInstancePostProcessor {
     }
 
     @Override
+    public void beforeAll(ExtensionContext context) {
+        // No-op
+    }
+
+    @Override
     public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
         Class<?> testClass = testInstance.getClass();
-        MockConfiguration mockConfig = testClass.getAnnotation(MockConfiguration.class);
-
-        if (mockConfig == null || mockConfig.enabled()) {
-            String mockDataFilePath = (mockConfig != null && !mockConfig.mockDataFilePath().isEmpty())
-                    ? mockConfig.mockDataFilePath() : null;
-            setupMockData(testClass, mockDataFilePath);
+        if (testClass.isAnnotationPresent(MockConfiguration.class)) {
+            MockConfiguration mockConfig = testClass.getAnnotation(MockConfiguration.class);
+            if (mockConfig.enabled()) {
+                String mockDataFilePath = mockConfig.mockDataFilePath();
+                setupMockData(testClass, mockDataFilePath);
+            }
         }
     }
 
-    private void setupMockData(Class<?> testClass, String overrideMockDataFilePath) {
+    private void setupMockData(Class<?> testClass, String mockDataFilePath) {
         try (InputStream configStream = testClass.getResourceAsStream("/mockdata/mock-config.json")) {
             if (configStream != null) {
                 String configContent = IOUtils.toString(configStream, "UTF-8");
                 Map<String, Map<String, String>> mockConfigMap = new ObjectMapper().readValue(configContent, HashMap.class);
                 mockConfigMap.forEach((endpoint, config) -> {
-                    String dataFilePath = (overrideMockDataFilePath != null) ? overrideMockDataFilePath : config.get("mockDataFilePath");
-                    String responseFormat = config.getOrDefault("responseFormat", DEFAULT_RESPONSE_FORMAT);
-                    String notFoundResponseFormat = config.getOrDefault("notFoundResponseFormat", DEFAULT_NOT_FOUND_RESPONSE_FORMAT);
-                    setupGraphQLStub(endpoint, dataFilePath, responseFormat, notFoundResponseFormat);
+                    String dataFilePath = config.get("mockDataFilePath");
+                    String responseFormat = config.get("responseFormat");
+                    String notFoundResponseFormat = config.get("notFoundResponseFormat");
+                    setupStub(endpoint, dataFilePath, responseFormat, notFoundResponseFormat);
                 });
             } else {
                 log.error("Mock configuration file not found");
@@ -59,33 +66,31 @@ public class GlobalWireMockExtension implements TestInstancePostProcessor {
         }
     }
 
-    private void setupGraphQLStub(String endpoint, String mockDataFilePath, String responseFormat, String notFoundResponseFormat) {
+    private void setupStub(String endpoint, String mockDataFilePath, String responseFormat, String notFoundResponseFormat) {
         try (InputStream mockDataStream = getClass().getResourceAsStream("/mockdata/" + mockDataFilePath);
              InputStream responseFormatStream = getClass().getResourceAsStream("/mockdata/" + responseFormat);
              InputStream notFoundResponseFormatStream = getClass().getResourceAsStream("/mockdata/" + notFoundResponseFormat)) {
-
-            String mockData = mockDataStream != null ? IOUtils.toString(mockDataStream, "UTF-8") : null;
-            String responseTemplate = responseFormatStream != null ? IOUtils.toString(responseFormatStream, "UTF-8") : responseFormat;
-            String notFoundResponseTemplate = notFoundResponseFormatStream != null ? IOUtils.toString(notFoundResponseFormatStream, "UTF-8") : notFoundResponseFormat;
-
-            if (mockData != null) {
+            if (mockDataStream != null && responseFormatStream != null && notFoundResponseFormatStream != null) {
+                String mockData = IOUtils.toString(mockDataStream, "UTF-8");
+                String responseTemplate = IOUtils.toString(responseFormatStream, "UTF-8");
+                String notFoundResponseTemplate = IOUtils.toString(notFoundResponseFormatStream, "UTF-8");
                 ObjectMapper objectMapper = new ObjectMapper();
                 List<Map<String, Object>> mockDataList = objectMapper.readValue(mockData, List.class);
 
+                String urlPattern = endpoint.replaceAll("\\{[^/]+}", "([^/]+)");
                 wireMockServer.stubFor(
-                    WireMock.post(WireMock.urlEqualTo(endpoint))
-                        .withHeader("Content-Type", WireMock.equalTo("application/json"))
+                    WireMock.get(WireMock.urlPathMatching(urlPattern))
                         .willReturn(WireMock.aResponse()
                             .withHeader("Content-Type", "application/json")
                             .withBody(request -> {
-                                int requestedId = getRequestedIdFromRequest(request.getBodyAsString());
+                                int requestedId = getRequestedIdFromRequest(request.getUrl(), urlPattern);
                                 return processRequest(mockDataList, responseTemplate, notFoundResponseTemplate, requestedId);
                             })
                         )
                 );
                 log.info("Stubbed endpoint {} with mock data from file {}", endpoint, mockDataFilePath);
             } else {
-                log.error("Mock data file not found: {}", mockDataFilePath);
+                log.error("Mock data file, response format file, or not found response format file not found: {}, {}, {}", mockDataFilePath, responseFormat, notFoundResponseFormat);
             }
         } catch (IOException e) {
             log.error("Failed to setup stub for endpoint {} with mock data file {}, response format {}, and not found response format {}: {}", endpoint, mockDataFilePath, responseFormat, notFoundResponseFormat, e.getMessage());
@@ -106,14 +111,13 @@ public class GlobalWireMockExtension implements TestInstancePostProcessor {
         return template.apply(errorResponse);
     }
 
-    private int getRequestedIdFromRequest(String requestBody) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, Object> requestMap = objectMapper.readValue(requestBody, HashMap.class);
-            Map<String, Object> variables = (Map<String, Object>) requestMap.get("variables");
-            return (int) variables.get("id");
-        } catch (IOException e) {
-            log.error("Failed to extract ID from request: {}", e.getMessage());
+    private int getRequestedIdFromRequest(String requestUrl, String urlPattern) {
+        Pattern pattern = Pattern.compile(urlPattern);
+        Matcher matcher = pattern.matcher(requestUrl);
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group(1));
+        } else {
+            log.error("Failed to extract ID from request URL: {}", requestUrl);
             return -1;
         }
     }
